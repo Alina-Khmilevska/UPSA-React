34239437d02067048273909d4d520d361f1b933a
Alina Hmilevska <a.khmilevska@internetdevels.com>
2023-09-27 14:36:18 +0300

    Fixed counter block

--- src/utils/ui/Counter.jsx
+++ src/utils/ui/Counter.jsx
@@ -3,7 +3,7 @@ import CountUp from "react-countup";
 
 export default function Counter({ number, title }) {
   return (
-    <div className="number">
+    <div className="text-center">
       <CountUp duration={5} className="counter font-semibold text-7xl leading-normal" end={number} />
       <h4 className="font-light text-5xl leading-normal ">{title}</h4>
     </div>
